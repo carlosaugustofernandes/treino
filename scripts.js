73faf932ace0ff5fa9d9c52739648fd5aaeee783
carlosaugustofernandes <carlos@veridoc.com.br>
2026-09-21 15:40:19 -0300

    Remove form submission handling from scripts.js

--- scripts.js
+++ scripts.js
@@ -1,10 +1,1 @@
-const form = document.getElementById("meuFormulario") as HTMLFormElement;
-const nomeInput = document.getElementById("nomeInput") as HTMLInputElement;
 
-form.addEventListener("submit", (event:SubmitEvent) => {
-    event.preventDefault();
-
-    const nome: string = nomeInput.value;
-    alert(`Formulários enviado! Olá, ${nome}`);
-    nomeInput.value = "";
-});
